636bf728badaa67364605642efea177179b5ade9
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-02 10:07:40 +0100

    nav icons + refactor into NavLinks

--- admin/src/components/Navbar.tsx
+++ admin/src/components/Navbar.tsx
@@ -1,14 +1,32 @@
 import { NavLink } from "react-router-dom";
+import { AiOutlineHome, AiFillFileAdd } from "react-icons/ai";
+import { ReactNode } from "react";
+
+interface INavItem {
+  to: string;
+  text: string;
+  icon: ReactNode;
+}
+
+const NavItem = ({ to, text, icon }: INavItem) => (
+  <NavLink className="flex items-center space-x-2" to={to}>
+    {icon} <span>{text}</span>
+  </NavLink>
+);
 
 const Navbar = () => {
   return (
     <nav>
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavItem to="/" text="Home" icon={<AiOutlineHome size={24} />} />
         </li>
         <li>
-          <NavLink to="/create-post">Create Post</NavLink>
+          <NavItem
+            to="/create-post"
+            text="Create Post"
+            icon={<AiFillFileAdd size={24} />}
+          />
         </li>
       </ul>
     </nav>
