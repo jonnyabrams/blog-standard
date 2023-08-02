7fc4c3bbf12a3184e4f31496349be836ec1ed601
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-02 09:51:55 +0100

    set up routes and basic nav links

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai";
+import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   const [closedNav, setClosedNav] = useState(false);
@@ -15,9 +21,9 @@ const App = () => {
   return (
     <div className="flex">
       {/* nav section  */}
-      <div
-        className={`${getNavWidth()} h-screen bg-red-400 transition-width`}
-      ></div>
+      <div className={`${getNavWidth()} h-screen bg-red-400 transition-width`}>
+        <Navbar />
+      </div>
 
       {/* content section */}
       <div className="flex-1 min-h-screen bg-blue-400">
@@ -28,6 +34,15 @@ const App = () => {
             <AiOutlineMenuFold size={25} />
           )}
         </button>
+
+        <div className="max-w-screen-lg mx-auto">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/update-post" element={<UpdatePost />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
