e7a074540d758e43c4e7d96c7cd3b064b1094ebe
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-02 08:24:46 +0100

    a bit of app cleanup

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,13 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
+const App = () => {
   return (
     <div>
       <h1 className="text-red-700">HELLO WORLD</h1>
     </div>
   );
-}
+};
 
 export default App;
