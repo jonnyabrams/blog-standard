6d02c033f9b56d584cfe681eb296acd5a3f914f8
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-03 07:56:03 +0100

    PostCard edit & delete buttons

--- admin/src/components/PostCard.tsx
+++ admin/src/components/PostCard.tsx
@@ -1,4 +1,5 @@
 import dateFormat from "dateformat";
+import { BsPencilSquare, BsTrash } from "react-icons/bs";
 
 import { PostType } from "../typings";
 
@@ -10,6 +11,9 @@ const PostCard = ({ post }: IPostCard) => {
   if (!post) return null;
   const { title, thumbnail, tags, meta, createdAt, slug } = post;
 
+  const iconClasses =
+    "flex justify-center items-center text-white w-8 h-8 rounded-full";
+
   return (
     <div className="bg-white shadow-sm rounded flex flex-col">
       <img
@@ -20,12 +24,22 @@ const PostCard = ({ post }: IPostCard) => {
       <div className="p-2 flex-1 flex flex-col justify-between">
         <h1 className="text-lg font-semibold text-gray-700">{title}</h1>
         <p className="text-gray-500 line-clamp-2">{meta}</p>
-        <div className="flex flex-col md:flex-row justify-between">
+        <div className="flex flex-col md:flex-row justify-between py-2">
           <p className="text-gray-500 text-sm">
             {dateFormat(createdAt, "mediumDate")}
           </p>
           <p className="text-gray-500 text-sm">{tags.join(", ")}</p>
         </div>
+      </div>
+      <div className="flex space-x-3 ml-2 mb-2">
+        <button className={`${iconClasses} bg-blue-400 hover:bg-blue-600`}>
+          <BsPencilSquare />
+        </button>
+        <button
+          className={`${iconClasses} text-white w-8 h-8 rounded-full bg-red-400 hover:bg-red-600`}
+        >
+          <BsTrash />
+        </button>
       </div>
     </div>
   );
