ff8ace0e2af7ad572de69afeef313230324c84cd
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-03 07:25:40 +0100

    more PostCard styles

--- admin/src/components/PostCard.tsx
+++ admin/src/components/PostCard.tsx
@@ -11,12 +11,18 @@ const PostCard = ({ post }: IPostCard) => {
   const { title, thumbnail, tags, meta, createdAt, slug } = post;
 
   return (
-    <div>
-      <img src={thumbnail?.url || "default.jpeg"} alt={title} />
-      <h1>{title}</h1>
-      <p>{meta}</p>
-      <p>{dateFormat(createdAt, "mediumDate")}</p>
-      <p>{tags.join(", ")}</p>
+    <div className="bg-white shadow-sm rounded">
+      <img className="rounded-t" src={thumbnail?.url || "default.jpeg"} alt={title} />
+      <div className="p-2">
+        <h1 className="text-lg font-semibold text-gray-700">{title}</h1>
+        <p className="text-gray-500">{meta}</p>
+        <div className="flex flex-col md:flex-row justify-between">
+          <p className="text-gray-500 text-sm">
+            {dateFormat(createdAt, "mediumDate")}
+          </p>
+          <p className="text-gray-500 text-sm">{tags.join(", ")}</p>
+        </div>
+      </div>
     </div>
   );
 };
