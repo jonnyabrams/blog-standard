31c367a460768747b0afd975b894d21ab40a491c
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-03 07:35:57 +0100

    truncate meta desc

--- admin/src/components/PostCard.tsx
+++ admin/src/components/PostCard.tsx
@@ -12,10 +12,14 @@ const PostCard = ({ post }: IPostCard) => {
 
   return (
     <div className="bg-white shadow-sm rounded">
-      <img className="rounded-t" src={thumbnail?.url || "default.jpeg"} alt={title} />
+      <img
+        className="rounded-t"
+        src={thumbnail?.url || "default.jpeg"}
+        alt={title}
+      />
       <div className="p-2">
         <h1 className="text-lg font-semibold text-gray-700">{title}</h1>
-        <p className="text-gray-500">{meta}</p>
+        <p className="text-gray-500 line-clamp-2">{meta}</p>
         <div className="flex flex-col md:flex-row justify-between">
           <p className="text-gray-500 text-sm">
             {dateFormat(createdAt, "mediumDate")}
