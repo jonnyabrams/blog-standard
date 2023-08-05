cc0188625277fddb87f30218361b9b79ef8c95b4
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-05 13:29:24 +0100

    Revert "can log out from dropdown"
    This reverts commit 4a2c00830ad0d20456941da44cf7f300016cb2cd.

--- admin/src/components/Header.tsx
+++ admin/src/components/Header.tsx
@@ -1,12 +1,6 @@
 import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai";
-import {
-  BiSolidChevronDownCircle,
-  BiSolidChevronUpCircle,
-} from "react-icons/bi";
-import { useContext, useState } from "react";
 
 import Searchbar from "./Searchbar";
-import { AuthContext } from "../context/AuthContext";
 
 interface IHeader {
   toggleNav: () => void;
@@ -14,23 +8,8 @@ interface IHeader {
 }
 
 const Header = ({ toggleNav, closedNav }: IHeader) => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  const { logout } = useContext(AuthContext);
-
-  const handleDropdownOpen = () => {
-    setDropdownOpen(!dropdownOpen);
-  };
-
-  const dropdownItem = (title: string, action: () => void, isLast = false) => (
-    <div className="hover:text-white hover:bg-[var(--main-blue)]">
-      <button onClick={action}>{title}</button>
-      {!isLast && <hr />}
-    </div>
-  );
-
   return (
-    <div className="sticky top-0 bg-white flex justify-between items-center pr-2">
+    <div className="sticky top-0 bg-white">
       <div className="flex items-center p-2 space-x-2">
         <button onClick={toggleNav}>
           {closedNav ? (
@@ -41,19 +20,6 @@ const Header = ({ toggleNav, closedNav }: IHeader) => {
         </button>
         <Searchbar />
       </div>
-      <button className="relative" onClick={handleDropdownOpen}>
-        {dropdownOpen ? (
-          <BiSolidChevronUpCircle size={25} />
-        ) : (
-          <BiSolidChevronDownCircle size={25} />
-        )}
-        {dropdownOpen ? (
-          <ul className="w-[150px] mt-1 rounded-sm bg-white border border-gray-400 pt-1 absolute top-5 left-[-120px]">
-            <li>{dropdownItem("Log Out", logout)}</li>
-            <li>{dropdownItem("Log Out", logout, true)}</li>
-          </ul>
-        ) : null}
-      </button>
     </div>
   );
 };
